0412f490786ad925a0ce9abd987fda456cb19d88
r1oga <r1oga@tuta.io>
2020-07-14 18:24:08 +0200

    feat: Init DB tables in Docker container

--- controllers/register.js
+++ controllers/register.js
@@ -1,11 +1,12 @@
 const handleRegister = (req, res, db, bcrypt) => {
-  const { email, name, password } = req.body;
+  const { email, name, password } = req.body
   if (!email || !name || !password) {
-    return res.status(400).json('incorrect form submission');
+    return res.status(400).json('incorrect form submission')
   }
-  const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      trx.insert({
+  const hash = bcrypt.hashSync(password)
+  db.transaction(trx => {
+    trx
+      .insert({
         hash: hash,
         email: email
       })
@@ -20,17 +21,14 @@ const handleRegister = (req, res, db, bcrypt) => {
             joined: new Date()
           })
           .then(user => {
-            res.json(user[0]);
+            res.json(user[0])
           })
       })
       .then(trx.commit)
       .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
+  }).catch(err => res.status(400).json('unable to register'))
 }
 
 module.exports = {
   handleRegister: handleRegister
-};
-
-
+}
---
+++ postgres/deploy_schemas.sql
@@ -0,0 +1,4 @@
+-- DEPLOY fresh database tables
+
+\i '/docker-entrypoint-initdb.d/tables/users.sql'
+\i '/docker-entrypoint-initdb.d/tables/login.sql'
