254b87d41ede308b021172a94ca774339c8f178e
Gauthier Riou <gauthier.riou@airbus.com>
2020-07-15 14:05:30 +0200

    feat: Get id when including token in header

--- controllers/signin.js
+++ controllers/signin.js
@@ -30,14 +30,20 @@ const handleSignin = (db, bcrypt, req, res) => {
     .catch(err => Promise.reject('Unable to find account with this email'))
 }
 
-const getAuthTokenId = () => {
-  console.log('auth ok')
-}
-
 const signToken = email =>
   jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '2 days' })
 
 const setKey = (key, value) => Promise.resolve(redisClient.set(key, value))
+
+const getAuthTokenId = (req, res) => {
+  const { authorization } = req.headers
+  return redisClient.get(authorization, (err, reply) => {
+    if (err || !reply) {
+      return res.status(400).json('Unauthorized')
+    }
+    return res.json({ id: reply })
+  })
+}
 
 const createSession = ({ email, id }) => {
   // create JWT token
@@ -51,7 +57,7 @@ const authenticate = (db, bcrypt) => (req, res) => {
   const { authorization } = req.headers
 
   return authorization
-    ? getAuthTokenId()
+    ? getAuthTokenId(req, res)
     : handleSignin(db, brypt, req, res)
         .then(data =>
           data.id && data.email ? createSession(data) : Promise.reject(data)
