221070c5ca2a97a48764e14865efc3871ea23d3b
r1oga <r1oga@tuta.io>
2020-07-19 14:09:22 +0200

    fix: Use PORT env var instead of fixed port

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ app.post('/imageurl', auth.requireAuth, (req, res) => {
   image.handleApiCall(req, res)
 })
 
-app.listen(3000, () => {
-  console.log('app is running on port 3000')
+app.listen(process.env.PORT, () => {
+  console.log('app is running')
 })
